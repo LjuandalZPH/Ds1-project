538cb509e6f15078d13775a5f35c04c674c667f7
Adrian Ordoñez <jose.adrian.marin@correounivalle.edu.co>
2024-11-01 20:03:39 -0500

    Register and Login completed, must implement with menú from template, admin view incompleted

--- Proyecto-desarrollo-1/LogRes/src/components/registerForm/RegisterForm.jsx
+++ Proyecto-desarrollo-1/LogRes/src/components/registerForm/RegisterForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './RegisterForm.css';
 import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
 
 const RegisterForm = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
@@ -13,33 +14,36 @@ const RegisterForm = () => {
         <body id="rbody">
             <div id="rcontainer">
                 <div id="presentation">
-                    <h2>Bienvenido a Unizone!</h2>
+                    <h2>Welcome to Unizone!</h2>
                 </div>
                 <form id="rform" onSubmit={handleSubmit(onSubmit)}>
-                    <h1>Registro</h1>
+                    <h1>Register</h1>
                     <div id="rinput-box">
-                        <input type="text" placeholder="Nombre" {...register('nombre', { required: true, maxLength: 20 })} />
+                        <input type="text" placeholder="Name" {...register('nombre', { required: true, maxLength: 20 })} />
                     </div>
                     <div id="rinput-box">
-                        <input type="text" placeholder="Dirección" {...register('direccion')} />
+                        <input type="text" placeholder="Address" {...register('direccion')} />
                     </div>
                     <div id="rinput-box">
-                        <input type="text" placeholder="Teléfono" {...register('telefono')} />
+                        <input type="text" placeholder="Phone" {...register('telefono')} />
                     </div>
                     <div id="rinput-box">
-                        <input type="text" placeholder="Correo" {...register('correo', { required: true })} />
+                        <input type="text" placeholder="Email" {...register('correo', { required: true })} />
                     </div>
                     <div id="rinput-box">
-                        <input type="password" placeholder="Contraseña" {...register('contrasena', { required: true })} />
+                        <input type="password" placeholder="password" {...register('contrasena', { required: true })} />
                     </div>
                     <div id="rinput-box">
-                        <label>Pais</label>
+                        <label>Country</label>
                         <select {...register('pais', { required: true })} id="country-dropdown">
                             <option value="co">Colombia</option>
                             <option value="us">United States</option>
                             <option value="fr">France</option>
                             <option value="ge">Germany</option>
                         </select>
+                    </div>
+                    <div id="login-link">
+                        <Link to="/" id="already-have-account">Already have an account?</Link>
                     </div>
                     <button id="button" type="submit">Send</button>
                 </form>
