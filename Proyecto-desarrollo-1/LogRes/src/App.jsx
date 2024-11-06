5ce8f8c13d97c6cd581e271bfc3553280baa5462
JuanferCalle <calle.juan@correounivalle.edu.co>
2024-11-06 11:56:15 -0500

    viewer main screen

--- Proyecto-desarrollo-1/LogRes/src/App.jsx
+++ Proyecto-desarrollo-1/LogRes/src/App.jsx
@@ -3,22 +3,24 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginForm from './components/loginForm/LoginForm';
-import RegisterForm from './components/registerForm/RegisterForm';
+import LoginForm from './components/loginForm/LoginForm'
+import RegisterForm from './components/registerForm/RegisterForm'
+import MainForm from './components/mainScreen/mainForm';
+
 
 function App() {
-  //const [count, setCount] = useState(0)
-
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route path="/" element={<MainForm />} />
+          <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
---
+++ Proyecto-desarrollo-1/LogRes/src/components/mainScreen/MainForm.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import './MainForm.css';
+import { Link } from 'react-router-dom';
+
+const MainForm = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    useEffect(() => {
+        document.title = "Unizone";
+    }, []);
+
+    return (
+        <>
+            {/* Encabezado principal */}
+            <header className="main-nav">
+                <Link className="brand" to="/">UNIZONE</Link>
+                <button className="menu-button" onClick={toggleMenu}>Menú</button>
+                <div className="search-bar">
+                    <input type="text" placeholder="Buscar en unizone.com" />
+                    <button className="search-button">🔍</button>
+                </div>
+                <div className="nav-icons">
+                    <Link to="/login">👤 Mi cuenta</Link>
+                    <Link to="/cart">🛒 Carrito</Link>
+                </div>
+            </header>
+
+            {/* Menú desplegable */}
+            {isMenuOpen && (
+                <div className="dropdown-menu">
+                    <ul className="menu-list">
+                        <li><Link to="/">Inicio</Link></li>
+                        <li><Link to="/about">Acerca de</Link></li>
+                        <li>Productos
+                            <ul>
+                                <li><Link to="/lista-productos">Lista de productos</Link></li>
+                                <li><Link to="/detalle-producto">Detalles del producto</Link></li>
+                            </ul>
+                        </li>
+                        <li>Páginas
+                            <ul>
+                                <li><Link to="/login">Iniciar sesión</Link></li>
+                                <li><Link to="/checkout">Pagar</Link></li>
+                                <li><Link to="/cart">Carrito de compras</Link></li>
+                                <li><Link to="/confirmacion">Confirmación</Link></li>
+                                <li><Link to="/elementos">Elementos</Link></li>
+                            </ul>
+                        </li>
+                        <li><Link to="/contacto">Contacto</Link></li>
+                    </ul>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default MainForm;
+
+
+
