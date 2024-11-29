f6707f08f4cae10e30d9212958ac6be7a59070cf
michaelRS2002 <148493262+michaelRS2002@users.noreply.github.com>
2024-11-28 23:23:59 -0500

    Update views.py

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+# Vista para listar y crear productos
+class ProductListCreate(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# Vista para obtener, actualizar y eliminar un producto específico
+class ProductRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
